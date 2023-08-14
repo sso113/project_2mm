edcb087d39abdc7e0f9c5fe52b307d3aacfdfad9
seojin <rlatjwls10403@naver.com>
2023-08-14 15:56:08 +0900

    연동

---
+++ project_2mm/project_2mm/accounts/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter, DefaultRouter
+
+
+from . import views
+from posts.views import GroupPostView, GroupPostDetailView
+
+urlpatterns = [
+    
+    #로그인 
+    path('api/login/', views.Loginview.as_view(), name='phone_login_view'),
+    path('api/logout/', views.LogoutView.as_view(), name='phone_logout_view'),
+
+    #소셜로그인 
+    path('authaccounts/', include('allauth.urls')),
+
+    #회원가입 
+    path('signup/', views.SingupView.as_view(), name='signup'),
+    path('update-password/', views.PasswordView.as_view(), name='update-password'),
+
+    # 모임
+    path('group/', views.GroupListCreateView.as_view(), name='group-list-create'),
+    path('group/<uuid:code>/', views.GroupDetailView.as_view(http_method_names=['get', 'patch', 'delete']), name='group-detail'),
+
+    # 마이페이지 
+    path('mypage/', views.MypageView.as_view(), name='mypage'),
+]
